dc768f2ca59fff3054020c156b2981405daf2d2c
renosang <renosang@gmail.com>
2016-06-10 20:50:20 +0700

    Hexagon Rom

---
+++ src/com/android/deskclock/SettingsActivity.java
@@ -0,0 +1,390 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2012-2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.deskclock;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import android.text.format.DateUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.android.deskclock.worldclock.Cities;
+
+import cyanogenmod.providers.CMSettings;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TimeZone;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+/**
+ * Settings for the Alarm Clock.
+ */
+public class SettingsActivity extends BaseActivity {
+
+    public static final String KEY_ALARM_SNOOZE = "snooze_duration";
+    public static final String KEY_ALARM_VOLUME = "volume_setting";
+    public static final String KEY_VOLUME_BEHAVIOR = "volume_button_setting";
+    public static final String KEY_FLIP_ACTION = "flip_action";
+    public static final String KEY_SHAKE_ACTION = "shake_action";
+    public static final String KEY_AUTO_SILENCE = "auto_silence";
+    public static final String KEY_CLOCK_STYLE = "clock_style";
+    public static final String KEY_HOME_TZ = "home_time_zone";
+    public static final String KEY_AUTO_HOME_CLOCK = "automatic_home_clock";
+    public static final String KEY_VOLUME_BUTTONS = "volume_button_setting";
+    public static final String KEY_SHOW_ALARM_ICON = "show_status_bar_icon";
+    public static final String KEY_ALARM_SETTINGS = "key_alarm_settings";
+    static final String KEY_DIGITAL_CLOCK_TIME_COLOR =
+            "digital_clock_time_color";
+    static final String KEY_DIGITAL_CLOCK_DATE_COLOR =
+            "digital_clock_date_color";
+    static final String KEY_DIGITAL_CLOCK_ALARM_COLOR =
+            "digital_clock_alarm_color";
+
+    public static final String DEFAULT_VOLUME_BEHAVIOR = "0";
+    public static final String VOLUME_BEHAVIOR_SNOOZE = "1";
+    public static final String VOLUME_BEHAVIOR_DISMISS = "2";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setVolumeControlStream(AudioManager.STREAM_ALARM);
+        setContentView(R.layout.settings);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        MenuItem help = menu.findItem(R.id.menu_item_help);
+        if (help != null) {
+            Utils.prepareHelpMenuItem(this, help);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public static class PrefsFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+
+        private static CharSequence[][] mTimezones;
+        private long mTime;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+
+            // We don't want to reconstruct the timezone list every single time onResume() is
+            // called so we do it once in onCreate
+            if (mTimezones == null) {
+                mTime = System.currentTimeMillis();
+                mTimezones = getAllTimezones();
+            }
+            final ListPreference homeTimezonePref = (ListPreference) findPreference(KEY_HOME_TZ);
+            homeTimezonePref.setEntryValues(mTimezones[0]);
+            homeTimezonePref.setEntries(mTimezones[1]);
+            homeTimezonePref.setSummary(homeTimezonePref.getEntry());
+            homeTimezonePref.setOnPreferenceChangeListener(this);
+
+            final SwitchPreference showAlarmIconPref =
+                    (SwitchPreference) findPreference(KEY_SHOW_ALARM_ICON);
+            showAlarmIconPref.setChecked(CMSettings.System.getInt(
+                    getActivity().getContentResolver(), CMSettings.System.SHOW_ALARM_ICON, 1) == 1);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            // By default, do not recreate the DeskClock activity
+            getActivity().setResult(RESULT_CANCELED);
+            refresh();
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference pref, Object newValue) {
+            if (KEY_AUTO_SILENCE.equals(pref.getKey())) {
+                final ListPreference autoSilencePref = (ListPreference) pref;
+                String delay = (String) newValue;
+                updateAutoSnoozeSummary(autoSilencePref, delay);
+            } else if (KEY_CLOCK_STYLE.equals(pref.getKey())) {
+                final ListPreference clockStylePref = (ListPreference) pref;
+                final int idx = clockStylePref.findIndexOfValue((String) newValue);
+                clockStylePref.setSummary(clockStylePref.getEntries()[idx]);
+            } else if (KEY_HOME_TZ.equals(pref.getKey())) {
+                final ListPreference homeTimezonePref = (ListPreference) pref;
+                final int idx = homeTimezonePref.findIndexOfValue((String) newValue);
+                homeTimezonePref.setSummary(homeTimezonePref.getEntries()[idx]);
+                notifyHomeTimeZoneChanged();
+            } else if (KEY_AUTO_HOME_CLOCK.equals(pref.getKey())) {
+                final boolean autoHomeClockEnabled = ((SwitchPreference) pref).isChecked();
+                final Preference homeTimeZonePref = findPreference(KEY_HOME_TZ);
+                homeTimeZonePref.setEnabled(!autoHomeClockEnabled);
+                notifyHomeTimeZoneChanged();
+            } else if (KEY_VOLUME_BUTTONS.equals(pref.getKey())) {
+                final ListPreference listPref = (ListPreference) pref;
+                updateActionSummary(listPref, (String) newValue, R.string.volume_buttons_summary);
+            } else if (KEY_FLIP_ACTION.equals(pref.getKey())) {
+                final ListPreference listPref = (ListPreference) pref;
+                updateActionSummary(listPref, (String) newValue, R.string.flip_action_summary);
+            } else if (KEY_SHAKE_ACTION.equals(pref.getKey())) {
+                final ListPreference listPref = (ListPreference) pref;
+                updateActionSummary(listPref, (String) newValue, R.string.shake_action_summary);
+            } else if (KEY_SHOW_ALARM_ICON.equals(pref.getKey())) {
+                CMSettings.System.putInt(getActivity().getContentResolver(),
+                        CMSettings.System.SHOW_ALARM_ICON, (Boolean) newValue ? 1 : 0);
+            } else if (KEY_DIGITAL_CLOCK_TIME_COLOR.equals(pref.getKey())
+                    || KEY_DIGITAL_CLOCK_DATE_COLOR.equals(pref.getKey())
+                    || KEY_DIGITAL_CLOCK_ALARM_COLOR.equals(pref.getKey())) {
+                AppWidgetManager widgetManager = AppWidgetManager.getInstance(getActivity());
+                int[] widgetIds = widgetManager.getAppWidgetIds(
+                        new ComponentName(getActivity(),
+                        com.android.alarmclock.DigitalAppWidgetProvider.class));
+                Intent update = new Intent(getActivity(),
+                com.android.alarmclock.DigitalAppWidgetProvider.class);
+                update.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                update.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, widgetIds);
+                getActivity().sendBroadcast(update);
+            }
+            // Set result so DeskClock knows to refresh itself
+            getActivity().setResult(RESULT_OK);
+            return true;
+        }
+
+        @Override
+        public boolean onPreferenceClick(Preference pref) {
+            final Activity activity = getActivity();
+            if (activity == null) {
+                return false;
+            }
+
+            if (KEY_ALARM_VOLUME.equals(pref.getKey())) {
+                final AudioManager audioManager =
+                        (AudioManager) activity.getSystemService(Context.AUDIO_SERVICE);
+                audioManager.adjustStreamVolume(AudioManager.STREAM_ALARM,
+                        AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * Returns an array of ids/time zones. This returns a double indexed array
+         * of ids and time zones for Calendar. It is an inefficient method and
+         * shouldn't be called often, but can be used for one time generation of
+         * this list.
+         *
+         * @return double array of tz ids and tz names
+         */
+        public CharSequence[][] getAllTimezones() {
+            Resources resources = this.getResources();
+            String[] ids = resources.getStringArray(R.array.timezone_values);
+            String[] labels = resources.getStringArray(R.array.timezone_labels);
+            int minLength = ids.length;
+            if (ids.length != labels.length) {
+                minLength = Math.min(minLength, labels.length);
+                LogUtils.e("Timezone ids and labels have different length!");
+            }
+            List<TimeZoneRow> timezones = new ArrayList<>();
+            for (int i = 0; i < minLength; i++) {
+                timezones.add(new TimeZoneRow(ids[i], labels[i]));
+            }
+            Collections.sort(timezones);
+
+            CharSequence[][] timeZones = new CharSequence[2][timezones.size()];
+            int i = 0;
+            for (TimeZoneRow row : timezones) {
+                timeZones[0][i] = row.mId;
+                timeZones[1][i++] = row.mDisplayName;
+            }
+            return timeZones;
+        }
+
+        private void refresh() {
+            final ListPreference autoSilencePref =
+                    (ListPreference) findPreference(KEY_AUTO_SILENCE);
+            String delay = autoSilencePref.getValue();
+            updateAutoSnoozeSummary(autoSilencePref, delay);
+            autoSilencePref.setOnPreferenceChangeListener(this);
+
+            final ListPreference clockStylePref = (ListPreference) findPreference(KEY_CLOCK_STYLE);
+            clockStylePref.setSummary(clockStylePref.getEntry());
+            clockStylePref.setOnPreferenceChangeListener(this);
+
+            final Preference autoHomeClockPref = findPreference(KEY_AUTO_HOME_CLOCK);
+            final boolean autoHomeClockEnabled =
+                    ((SwitchPreference) autoHomeClockPref).isChecked();
+            autoHomeClockPref.setOnPreferenceChangeListener(this);
+
+            final ListPreference homeTimezonePref = (ListPreference) findPreference(KEY_HOME_TZ);
+            homeTimezonePref.setEnabled(autoHomeClockEnabled);
+            homeTimezonePref.setSummary(homeTimezonePref.getEntry());
+            homeTimezonePref.setOnPreferenceChangeListener(this);
+
+            final ListPreference volumeButtonsPref = (ListPreference) findPreference(KEY_VOLUME_BUTTONS);
+            updateActionSummary(volumeButtonsPref, volumeButtonsPref.getValue(), R.string.volume_buttons_summary);
+            volumeButtonsPref.setOnPreferenceChangeListener(this);
+
+            SensorManager sensorManager = (SensorManager)
+                    getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+            final ListPreference flipActionPref = (ListPreference) findPreference(KEY_FLIP_ACTION);
+            if (flipActionPref != null) {
+                if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) {
+                    flipActionPref.setValue("0"); // Turn it off
+                    PreferenceCategory category = (PreferenceCategory) findPreference(KEY_ALARM_SETTINGS);
+                    if (category != null) {
+                        category.removePreference(flipActionPref);
+                    }
+                } else {
+                    updateActionSummary(flipActionPref, flipActionPref.getValue(), R.string.flip_action_summary);
+                    flipActionPref.setOnPreferenceChangeListener(this);
+                }
+            }
+
+            final ListPreference shakeActionPref = (ListPreference) findPreference(KEY_SHAKE_ACTION);
+            if (shakeActionPref != null) {
+                List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+                if (sensorList.size() < 1) { // This will be true if no accelerometer sensor
+                    shakeActionPref.setValue("0"); // Turn it off
+                    PreferenceCategory category = (PreferenceCategory) findPreference(KEY_ALARM_SETTINGS);
+                    if (category != null) {
+                        category.removePreference(shakeActionPref);
+                    }
+                } else {
+                    updateActionSummary(shakeActionPref, shakeActionPref.getValue(), R.string.shake_action_summary);
+                    shakeActionPref.setOnPreferenceChangeListener(this);
+                }
+            }
+
+            final Preference volumePref = findPreference(KEY_ALARM_VOLUME);
+            volumePref.setOnPreferenceClickListener(this);
+
+            final SnoozeLengthDialog snoozePref =
+                    (SnoozeLengthDialog) findPreference(KEY_ALARM_SNOOZE);
+            snoozePref.setSummary();
+
+            final SwitchPreference showAlarmIconPref =
+                    (SwitchPreference) findPreference(KEY_SHOW_ALARM_ICON);
+            showAlarmIconPref.setOnPreferenceChangeListener(this);
+
+            ColorPickerPreference clockTimeColor = (ColorPickerPreference)
+                    findPreference(KEY_DIGITAL_CLOCK_TIME_COLOR);
+            clockTimeColor.setOnPreferenceChangeListener(this);
+
+            ColorPickerPreference clockDateColor = (ColorPickerPreference)
+                    findPreference(KEY_DIGITAL_CLOCK_DATE_COLOR);
+            clockDateColor.setOnPreferenceChangeListener(this);
+
+            ColorPickerPreference clockAlarmColor = (ColorPickerPreference)
+                    findPreference(KEY_DIGITAL_CLOCK_ALARM_COLOR);
+            clockAlarmColor.setOnPreferenceChangeListener(this);
+        }
+
+        private void updateAutoSnoozeSummary(ListPreference listPref, String delay) {
+            int i = Integer.parseInt(delay);
+            if (i == -1) {
+                listPref.setSummary(R.string.auto_silence_never);
+            } else {
+                listPref.setSummary(Utils.getNumberFormattedQuantityString(getActivity(),
+                        R.plurals.auto_silence_summary, i));
+            }
+        }
+
+        private void notifyHomeTimeZoneChanged() {
+            Intent i = new Intent(Cities.WORLDCLOCK_UPDATE_INTENT);
+            getActivity().sendBroadcast(i);
+        }
+
+        private void updateActionSummary(ListPreference listPref, String action, int summaryResId) {
+            int i = listPref.findIndexOfValue(action);
+            listPref.setSummary(getString(summaryResId,
+                    getResources().getStringArray(R.array.action_summary_entries)[i]));
+        }
+
+        private class TimeZoneRow implements Comparable<TimeZoneRow> {
+            private static final boolean SHOW_DAYLIGHT_SAVINGS_INDICATOR = false;
+
+            public final String mId;
+            public final String mDisplayName;
+            public final int mOffset;
+
+            public TimeZoneRow(String id, String name) {
+                mId = id;
+                TimeZone tz = TimeZone.getTimeZone(id);
+                boolean useDaylightTime = tz.useDaylightTime();
+                mOffset = tz.getOffset(mTime);
+                mDisplayName = buildGmtDisplayName(name, useDaylightTime);
+            }
+
+            @Override
+            public int compareTo(TimeZoneRow another) {
+                return mOffset - another.mOffset;
+            }
+
+            public String buildGmtDisplayName(String displayName, boolean useDaylightTime) {
+                int p = Math.abs(mOffset);
+                StringBuilder name = new StringBuilder("(GMT");
+                name.append(mOffset < 0 ? '-' : '+');
+
+                name.append(p / DateUtils.HOUR_IN_MILLIS);
+                name.append(':');
+
+                int min = p / 60000;
+                min %= 60;
+
+                if (min < 10) {
+                    name.append('0');
+                }
+                name.append(min);
+                name.append(") ");
+                name.append(displayName);
+                if (useDaylightTime && SHOW_DAYLIGHT_SAVINGS_INDICATOR) {
+                    name.append(" \u2600"); // Sun symbol
+                }
+                return name.toString();
+            }
+        }
+    }
+}
